4cad8598cece69ca4c7123ef89c3a4abb8c4ba94
Javier-Reyes-pet <javier.reyes@laika.pet>
2023-11-25 21:47:49 -0500

    se crea registro de usuario

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
 
 class UserController extends Controller
 {
@@ -12,14 +15,67 @@ class UserController extends Controller
     }
 
     public function register(Request $request){
+        $data =  [
+            'status'=>'succcess',
+            'code'  => 200,
+            'msg'   => 'El usuario se ha creado correctamente'
+        ];
+        // validar datos
+        $validate = $this->validateRequest($request,[
+            'name'     => 'required|alpha'/* alfabetico */,
+            'surname'  => 'required|alpha',
+            'email'    => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
 
+        if($validate['error']){
+            $data =  [
+                'status'=>'error',
+                'code'  => 404,
+                'msg'   => $validate['msg'],
+                'user'  => []
+            ];
+        }
 
+        $password = password_hash($request->password, PASSWORD_BCRYPT, ['cost' =>4]);
+        $user     = new User;
 
-
-        return "Acion de registro de usuario  $request->name";
+        $user->name     = $request->name;
+        $user->surname  = $request->surname;
+        $user->email    = $request->email;
+        $user->password = $password;
+        $user->role     = 'ROLE_USER';
+        $user->save();
+        
+        $data['user'] = $user;
+        return response()->json($data, $data['code']);
     }
 
     public function login(Request $request){
         return "Accion login";
+    }
+
+
+    public function validateRequest($request, $rules){
+        $error = false;
+        $msg    ='ok';
+
+        $validator =Validator::make($request->all(), $rules,
+        [
+            'required'    => "El campo ':attribute' es requerido.",
+            'required_if' => "El campo ':attribute' es requerido cuando el role_id es 26.",
+            'numeric'     => "El campo ':attribute' debe ser de tipo númerico.",
+            'date'        => "El campo ':attribute' debe ser de tipo fecha.",
+            'min'         => "El campo ':attribute' debe ser mayor o igual a :min.",
+            'date_format' => "El campo ':attribute' debe tener el formato YYYY-mm-dd.",
+            'in'          => "El valor de ':attribute' no es válido"
+        ]
+        );
+        if($validator->fails()){
+           $error  = true;
+           $msg    = array_values($validator->errors()->toArray())[0][0] ??'error request';
+        }
+
+        return compact('error','msg');
     }
 }
