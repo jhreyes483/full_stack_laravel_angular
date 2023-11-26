91a2569bfbf1d64dbb07c6ec5510be58e4f3d578
Javier-Reyes-pet <javier.reyes@laika.pet>
2023-11-26 16:27:09 -0500

    CORS permite peticiones de dominios distintos

--- public/index.php
+++ public/index.php
@@ -3,6 +3,18 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+/** permite peticiones de dominios distintos  */
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+/******************************************** */
+
+
 define('LARAVEL_START', microtime(true));
 
 /*
