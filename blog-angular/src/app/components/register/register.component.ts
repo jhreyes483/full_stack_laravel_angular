71059271b5276bfb92f749bc5029d65c5ceea793
Javier-Reyes-pet <javier.reyes@laika.pet>
2023-11-27 10:09:23 -0500

    se crean validaciones iniciales de formulario

--- blog-angular/src/app/components/register/register.component.ts
+++ blog-angular/src/app/components/register/register.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { User } from '../../models/user';
+
 
 @Component({
   selector: 'register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
- componentename : string ='register';
+  page_title: string = 'Registrate';
+  public user: User;
+  form :any[]=[];
+
+  constructor() {
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
+  }
+
+  onSubmit( form :any){
+    console.log('funcion onSubmit')
+  }
+
 }
