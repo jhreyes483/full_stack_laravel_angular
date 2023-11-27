d8e5405a8a8a2826d6ac1da405428e763dbfec17
Javier-Reyes-pet <javier.reyes@laika.pet>
2023-11-26 21:54:11 -0500

    configuracion de componente rester

--- blog-angular/src/app/app.component.ts
+++ blog-angular/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent],
+  imports: [CommonModule, RouterOutlet, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- blog-angular/src/app/components/register/register.component.ts
+++ blog-angular/src/app/components/register/register.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-register',
+  selector: 'register',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+ componentename : string ='register';
 }
