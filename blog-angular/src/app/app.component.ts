2bc537ce93a756130ff1fca213ff00705ef753a8
Javier-Reyes-pet <javier.reyes@laika.pet>
2023-11-26 22:48:50 -0500

    configuracion inicial de routes

--- blog-angular/src/app/app.component.ts
+++ blog-angular/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet
+  ],
+  providers:[],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- blog-angular/src/app/app.routes.ts
+++ blog-angular/src/app/app.routes.ts
@@ -1,3 +1,34 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+//IMPORSTAR COMPONENTES
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+// DEFINIR RUTAS
+export const routes: Routes = [
+    {
+        path: '',
+        component: LoginComponent,
+    },
+    {
+        path: 'inicio',
+        component: LoginComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'registro',
+        component: RegisterComponent
+    }
+];
+
+@NgModule({
+    declarations: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+// EXPORTAR CONFIGURACION
+export class AppRoutingModule {}
