99c367e3b74cf6dd74f1e7660c825d5306eda3f1
Javier-Reyes-pet <javier.reyes@laika.pet>
2023-11-28 11:57:30 -0500

    creacion de compomnente user-edit ng g component components/user-edit

---
+++ blog-angular/src/app/components/user-edit/user-edit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-edit',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-edit.component.html',
+  styleUrl: './user-edit.component.css'
+})
+export class UserEditComponent {
+
+}
