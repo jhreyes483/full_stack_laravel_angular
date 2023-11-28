e40175ededc951a7186efb95f11609af7db624c7
Javier-Reyes-pet <javier.reyes@laika.pet>
2023-11-28 12:32:38 -0500

    html inicial de edit-user

--- blog-angular/src/app/components/user-edit/user-edit.component.ts
+++ blog-angular/src/app/components/user-edit/user-edit.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-user-edit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule,  ReactiveFormsModule],
   templateUrl: './user-edit.component.html',
   styleUrl: './user-edit.component.css'
 })
 export class UserEditComponent {
+  public page_title :string;
+  public status : string;
+  public user : User;
+
+
+  constructor(){
+    this.page_title = 'Ajustes';
+    this.status = '';
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
+  }
+
+  onSubmit(form : any){
+    
+  }
 
 }
